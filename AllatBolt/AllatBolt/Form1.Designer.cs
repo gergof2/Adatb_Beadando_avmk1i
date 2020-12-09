9b7e3c58adba8a6d31aa560c5f72b50dd2ac35e8
gergof2 <71332043+gergof2@users.noreply.github.com>
2020-12-09 14:01:20 +0100

    WinForm

--- AllatBolt/AllatBolt/Form1.Designer.cs
+++ AllatBolt/AllatBolt/Form1.Designer.cs
@@ -29,26 +29,17 @@ namespace AllatBolt
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgv_allatok = new System.Windows.Forms.DataGridView();
             this.b_insert = new System.Windows.Forms.Button();
-            this.b_change = new System.Windows.Forms.Button();
             this.b_delete = new System.Windows.Forms.Button();
             this.tb_id = new System.Windows.Forms.TextBox();
             this.tb_faj = new System.Windows.Forms.TextBox();
-            this.tb_nem = new System.Windows.Forms.TextBox();
-            this.tb_etkezes = new System.Windows.Forms.TextBox();
             this.tb_ar = new System.Windows.Forms.TextBox();
             this.tb_bolt = new System.Windows.Forms.TextBox();
+            this.cb_nem = new System.Windows.Forms.ComboBox();
+            this.cb_etkezes = new System.Windows.Forms.ComboBox();
+            this.dgv_allatok = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_allatok)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgv_allatok
-            // 
-            this.dgv_allatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_allatok.Location = new System.Drawing.Point(13, 13);
-            this.dgv_allatok.Name = "dgv_allatok";
-            this.dgv_allatok.Size = new System.Drawing.Size(775, 294);
-            this.dgv_allatok.TabIndex = 0;
             // 
             // b_insert
             // 
@@ -58,24 +49,17 @@ namespace AllatBolt
             this.b_insert.TabIndex = 1;
             this.b_insert.Text = "Hozzáadás";
             this.b_insert.UseVisualStyleBackColor = true;
-            // 
-            // b_change
-            // 
-            this.b_change.Location = new System.Drawing.Point(12, 350);
-            this.b_change.Name = "b_change";
-            this.b_change.Size = new System.Drawing.Size(116, 30);
-            this.b_change.TabIndex = 2;
-            this.b_change.Text = "Módosítás";
-            this.b_change.UseVisualStyleBackColor = true;
+            this.b_insert.Click += new System.EventHandler(this.b_insert_Click);
             // 
             // b_delete
             // 
-            this.b_delete.Location = new System.Drawing.Point(12, 386);
+            this.b_delete.Location = new System.Drawing.Point(13, 350);
             this.b_delete.Name = "b_delete";
             this.b_delete.Size = new System.Drawing.Size(116, 30);
             this.b_delete.TabIndex = 3;
             this.b_delete.Text = "Törlés";
             this.b_delete.UseVisualStyleBackColor = true;
+            this.b_delete.Click += new System.EventHandler(this.b_delete_Click);
             // 
             // tb_id
             // 
@@ -93,22 +77,6 @@ namespace AllatBolt
             this.tb_faj.TabIndex = 5;
             this.tb_faj.Text = "Fajta";
             // 
-            // tb_nem
-            // 
-            this.tb_nem.Location = new System.Drawing.Point(480, 314);
-            this.tb_nem.Name = "tb_nem";
-            this.tb_nem.Size = new System.Drawing.Size(153, 20);
-            this.tb_nem.TabIndex = 6;
-            this.tb_nem.Text = "Neme";
-            // 
-            // tb_etkezes
-            // 
-            this.tb_etkezes.Location = new System.Drawing.Point(162, 350);
-            this.tb_etkezes.Name = "tb_etkezes";
-            this.tb_etkezes.Size = new System.Drawing.Size(153, 20);
-            this.tb_etkezes.TabIndex = 7;
-            this.tb_etkezes.Text = "Mit eszik?";
-            // 
             // tb_ar
             // 
             this.tb_ar.Location = new System.Drawing.Point(322, 350);
@@ -125,23 +93,48 @@ namespace AllatBolt
             this.tb_bolt.TabIndex = 9;
             this.tb_bolt.Text = "Melyik bolt? (ID)";
             // 
+            // cb_nem
+            // 
+            this.cb_nem.FormattingEnabled = true;
+            this.cb_nem.Location = new System.Drawing.Point(481, 314);
+            this.cb_nem.Name = "cb_nem";
+            this.cb_nem.Size = new System.Drawing.Size(152, 21);
+            this.cb_nem.TabIndex = 10;
+            // 
+            // cb_etkezes
+            // 
+            this.cb_etkezes.FormattingEnabled = true;
+            this.cb_etkezes.Location = new System.Drawing.Point(162, 350);
+            this.cb_etkezes.Name = "cb_etkezes";
+            this.cb_etkezes.Size = new System.Drawing.Size(153, 21);
+            this.cb_etkezes.TabIndex = 11;
+            // 
+            // dgv_allatok
+            // 
+            this.dgv_allatok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_allatok.Location = new System.Drawing.Point(13, 13);
+            this.dgv_allatok.Name = "dgv_allatok";
+            this.dgv_allatok.RowTemplate.Height = 6;
+            this.dgv_allatok.Size = new System.Drawing.Size(620, 294);
+            this.dgv_allatok.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 436);
+            this.ClientSize = new System.Drawing.Size(647, 436);
+            this.Controls.Add(this.cb_etkezes);
+            this.Controls.Add(this.cb_nem);
             this.Controls.Add(this.tb_bolt);
             this.Controls.Add(this.tb_ar);
-            this.Controls.Add(this.tb_etkezes);
-            this.Controls.Add(this.tb_nem);
             this.Controls.Add(this.tb_faj);
             this.Controls.Add(this.tb_id);
             this.Controls.Add(this.b_delete);
-            this.Controls.Add(this.b_change);
             this.Controls.Add(this.b_insert);
             this.Controls.Add(this.dgv_allatok);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_allatok)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -149,17 +142,15 @@ namespace AllatBolt
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgv_allatok;
         private System.Windows.Forms.Button b_insert;
-        private System.Windows.Forms.Button b_change;
         private System.Windows.Forms.Button b_delete;
         private System.Windows.Forms.TextBox tb_id;
         private System.Windows.Forms.TextBox tb_faj;
-        private System.Windows.Forms.TextBox tb_nem;
-        private System.Windows.Forms.TextBox tb_etkezes;
         private System.Windows.Forms.TextBox tb_ar;
         private System.Windows.Forms.TextBox tb_bolt;
+        private System.Windows.Forms.ComboBox cb_nem;
+        private System.Windows.Forms.ComboBox cb_etkezes;
+        private System.Windows.Forms.DataGridView dgv_allatok;
     }
 }
 
--- AllatBolt/AllatBolt/Form1.cs
+++ AllatBolt/AllatBolt/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AllatBolt.Models.Manager;
+using AllatBolt.Models.Records;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,168 @@ namespace AllatBolt
 {
     public partial class Form1 : Form
     {
+        AllatokTabla tablaManager;
+        List<Allatok> rekords_AllatokList;
+        BackgroundWorker bgWorker;
         public Form1()
         {
             InitializeComponent();
+            tablaManager = new AllatokTabla();
+            rekords_AllatokList = new List<Allatok>();
+            bgWorker = new BackgroundWorker();
+        }
+
+        private void b_delete_Click(object sender, EventArgs e)
+        {
+            int ToroltSorok = 0;
+            foreach(DataGridViewRow selectedRows in dgv_allatok.SelectedRows)
+            {
+                Allatok TorlendoRekord = new Allatok();
+                TorlendoRekord.Allat_id = selectedRows.Cells["idszam"].Value.ToString();
+
+                ToroltSorok += tablaManager.Delete(TorlendoRekord);
+            }
+
+            MessageBox.Show(string.Format("{0} sor lett törölve!", ToroltSorok));
+            if (ToroltSorok != 0)
+                bgWorker.RunWorkerAsync();
+        }
+
+        private void b_insert_Click(object sender, EventArgs e)
+        {
+            Allatok allat = new Allatok()
+            {
+                Allat_id = tb_id.Text,
+                Faj = tb_faj.Text,
+                Nem = cb_nem.SelectedItem.ToString(),
+                Etkezes = cb_etkezes.SelectedItem.ToString(),
+                Ar = int.Parse(tb_ar.Text),
+                Bolt = tb_bolt.Text
+            };
+
+            tablaManager.Insert(allat);
+            bgWorker.RunWorkerAsync();
+
+            MessageBox.Show("Sikeres feltöltés!");
+            tb_id.Clear();
+            tb_faj.Clear();
+            tb_ar.Clear();
+            tb_bolt.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bgWorker.WorkerSupportsCancellation = true;
+            cb_nem.DataSource = Enum.GetValues(typeof(Nem));
+            cb_etkezes.DataSource = Enum.GetValues(typeof(Etkezes));
+
+            InitDataGridView();
+        }
+
+        private void InitDataGridView()
+        {
+            dgv_allatok.Rows.Clear();
+            dgv_allatok.Columns.Clear();
+            var allat = this.rekords_AllatokList;
+            dgv_allatok.DataSource = allat;
+
+            //DataGridViewColumn IdColumn = new DataGridViewColumn()
+            //{
+            //    Name = "id",
+            //    HeaderText = "ID",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(IdColumn);
+            //
+            //DataGridViewColumn FajColumn = new DataGridViewColumn()
+            //{
+            //    Name = "faj",
+            //    HeaderText = "Faj",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(FajColumn);
+            //
+            //DataGridViewColumn NemColumn = new DataGridViewColumn()
+            //{
+            //    Name = "nem",
+            //    HeaderText = "Nem",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(NemColumn);
+            //
+            //DataGridViewColumn EtkezesColumn = new DataGridViewColumn()
+            //{
+            //    Name = "etkezes",
+            //    HeaderText = "Étkezés",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(EtkezesColumn);
+            //
+            //DataGridViewColumn ArColumn = new DataGridViewColumn()
+            //{
+            //    Name = "ar",
+            //    HeaderText = "Ár",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(ArColumn);
+            //
+            //DataGridViewColumn BoltColumn = new DataGridViewColumn()
+            //{
+            //    Name = "bolt",
+            //    HeaderText = "Bolt",
+            //    Visible = true,
+            //    AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
+            //};
+            //dgv_allatok.Columns.Add(BoltColumn);
+        }
+
+
+        private void BgWorker_RunWorkerComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            FillDataGridView();
+        }
+
+        private void BgWorekr_DoWork(object sender, DoWorkEventArgs e)
+        {
+            rekords_AllatokList = tablaManager.Select();
+        }
+
+        private void FillDataGridView()
+        {
+            DataGridViewRow[] dataGridViewRows = new DataGridViewRow[rekords_AllatokList.Count];
+
+            for(int i = 0; i< rekords_AllatokList.Count; i++)
+            {
+                DataGridViewRow dataGridViewRow = new DataGridViewRow();
+
+                DataGridViewCell IdCell = new DataGridViewTextBoxCell();
+                IdCell.Value = rekords_AllatokList[i].Allat_id;
+                dataGridViewRow.Cells.Add(IdCell);
+
+                DataGridViewCell BoltCell = new DataGridViewTextBoxCell();
+                BoltCell.Value = rekords_AllatokList[i].Bolt;
+                dataGridViewRow.Cells.Add(BoltCell);
+
+                DataGridViewCell NemCell = new DataGridViewTextBoxCell();
+                NemCell.Value = rekords_AllatokList[i].Nem;
+                dataGridViewRow.Cells.Add(NemCell);
+
+                dataGridViewRows[i] = dataGridViewRow;
+            }
+            dgv_allatok.Rows.Clear();
+            dgv_allatok.Rows.AddRange(dataGridViewRows);
+        }
+
+        private void tb_id_Leave(object sender, EventArgs e)
+        {
+            string actual = tb_id.Text;
+            bool Correct = tablaManager.CheckIdszam(actual);
+            tb_id.BackColor = Correct ? Color.White : Color.Red;
         }
     }
 }
